37f8b6bfc96fa5ad770489435af455e65e51806a
Joshua Book <joshua.book@gmail.com>
2015-01-21 07:49:10 -0800

    deployment heroku stuff

---
+++ web.js
@@ -0,0 +1,20 @@
+var express = require('express');
+
+// var modRewrite = require('connect-modrewrite');
+var port = process.env.PORT || 9000;
+
+var app = express();
+app.use(express.static(__dirname + '/dist'));
+
+/* Rewrite every route to / so Angular can pick it up */
+// app.use(modRewrite([
+//   '^(.*)$ / [L]'
+// ]));
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/dist/index.html');
+});
+
+app.listen(port, function(){
+  console.log('Express server listening on port ' + port);
+});
